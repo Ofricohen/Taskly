40707e0a0c3fb6f678bb3c485459cd495ade42f5
Ofricohen <Ofricohen12345@gmail.com>
2026-06-24 16:13:21 +0300

    Add Microsoft Clarity

--- src/main.jsx
+++ src/main.jsx
@@ -1,8 +1,14 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
+import Clarity from "@microsoft/clarity";
+
 import "./index.css";
 import App from "./App.jsx";
 import "./styles/globals.css";
+
+if (import.meta.env.PROD) {
+  Clarity.init("xc20ieuf9x");
+}
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
